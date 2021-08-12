f9ea4bc57f903ee2809f93b4d86c5d3b5c6e8897
Dylan Cole <logboarddjc@gmail.com>
2021-08-12 19:31:45 -0400

    clean up battlefieldjs

--- client/src/pages/battlefield.js
+++ client/src/pages/battlefield.js
@@ -8,13 +8,9 @@ import '../assets/css/reset.css';
 
 function Battlefield() {
     return (
-        // <div className='char-div'>
         <div className='background char-div' style={{backgroundImage:`url(${background})`}}>
-            {/* <div className='char-div'> */}
-                {/* <img className='background' src={background} alt='landscape'/> */}
                 <img className=' left character' src={character} alt='character'/>
                 <img className='right character' src={character2} alt='character2'/>
-            {/* </div> */}
         </div>
     );
 }
